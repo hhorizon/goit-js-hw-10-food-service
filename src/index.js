1bacb8ef621dfa99ef1f6df83c69b3b0f8b9f82f
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2021-10-10 16:00:46 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -15,29 +15,29 @@ function createMenu(menu) {
 
 // ====== Переключатель темы ======
 
-// const theme = {
-//     LIGHT: 'light-theme',
-//     DARK: 'dark-theme'
-// }
-// const inputRef = document.querySelector('.theme-switch__toggle');
-// const bodyRef = document.querySelector('body');
-// const labelRef = document.querySelector('.theme-switch__track');
+const theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme'
+}
+const inputRef = document.querySelector('.theme-switch__toggle');
+const bodyRef = document.querySelector('body');
+const labelRef = document.querySelector('.theme-switch__track');
 
 // defaultTheme();
 
-// inputRef.addEventListener('change', onThemeSwitcChange);
+inputRef.addEventListener('change', onThemeSwitcChange);
 
-// function onThemeSwitcChange() {
-//     if (inputRef.checked) {
-//         bodyRef.classList.add(theme.DARK);
-//         bodyRef.classList.remove(theme.LIGHT);
-//     } else {
-//         bodyRef.classList.remove(theme.DARK);
-//         bodyRef.classList.add(theme.LIGHT);
-//     }
+function onThemeSwitcChange() {
+    if (inputRef.checked) {
+        bodyRef.classList.add(theme.DARK);
+        bodyRef.classList.remove(theme.LIGHT);
+    } else {
+        bodyRef.classList.remove(theme.DARK);
+        bodyRef.classList.add(theme.LIGHT);
+    }
 
-//     localStorage.setItem('theme', bodyRef.classList.value);
-// };
+    // localStorage.setItem('theme', bodyRef.classList.value);
+};
 
 // function defaultTheme() {
 //     bodyRef.classList.add(localStorage.getItem('theme'));
