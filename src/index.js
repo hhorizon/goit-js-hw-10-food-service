2158b6c7d89ab5c3487c6e312f2ca40007783a3f
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2021-10-13 22:34:18 +0300

    v3

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const theme = {
 }
 const inputRef = document.querySelector('.theme-switch__toggle');
 const bodyRef = document.querySelector('body');
-const labelRef = document.querySelector('.theme-switch__track');
 
 defaultTheme();
 
@@ -31,16 +30,20 @@ function onThemeSwitcChange() {
     if (inputRef.checked) {
         bodyRef.classList.add(theme.DARK);
         bodyRef.classList.remove(theme.LIGHT);
+        localStorage.setItem('theme', theme.DARK);
     } else {
         bodyRef.classList.remove(theme.DARK);
         bodyRef.classList.add(theme.LIGHT);
+        localStorage.setItem('theme', theme.LIGHT);
     }
-
-    localStorage.setItem('theme', bodyRef.classList.value);
 };
 
 function defaultTheme() {
-    bodyRef.classList.add(localStorage.getItem('theme'));
+    if (!localStorage.getItem('theme')) {
+        localStorage.setItem('theme', theme.LIGHT);
+    } else {
+        bodyRef.classList.add(localStorage.getItem('theme'));
+    };    
 
     if (localStorage.getItem('theme') === theme.DARK) {
        inputRef.checked = true; 
